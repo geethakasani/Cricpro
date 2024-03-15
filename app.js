c9d96016567f419ac46aad4ce83f7cb9b69ee872
geethakasani <160598661+geethakasani@users.noreply.github.com>
2024-03-15 22:27:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+/*const sehwagimg=document.getElementById("sehwag-img");
+const imgClick=document.getElementById("sehwag");
+
+imgClick.addEventListener("click",function(){
+    sehwagimg.style.display="block"  
+})*/
+const sehwagimg = document.getElementById("sehwag-img");
+const sehwag = document.getElementById("sehwag");
+sehwag.addEventListener("click", function () {
+  sehwagimg.style.display = "block";
+});
+
+const sachinimg = document.getElementById("sachin-img");
+const sachin = document.getElementById("Sachin");
+sachin.addEventListener("click", function () {
+  sachinimg.style.display = "block";
+});
+
+const gambhirimg = document.getElementById("gambhir-img");
+const gambhir = document.getElementById("Gautam");
+gambhir.addEventListener("click", function () {
+  gambhirimg.style.display = "block";
+});
+
+const kohliimg = document.getElementById("kohli-img");
+const kohli = document.getElementById("virat");
+kohli.addEventListener("click", function () {
+  kohliimg.style.display = "block";
+});
+
+const dhoniimg = document.getElementById("dhoni-img");
+const dhoni = document.getElementById("Dhoni");
+dhoni.addEventListener("click", function () {
+  dhoniimg.style.display = "block";
+});
+
+const yuvrajimg = document.getElementById("yuvraj-img");
+const yuvraj = document.getElementById("Yuvraj");
+yuvraj.addEventListener("click", function () {
+  yuvrajimg.style.display = "block";
+});
+
+const raina = document.getElementById("Suresh Raina");
+  raina.addEventListener("click", function () {
+    document.getElementById("raina-img").style.display = "block";
+  });
+
+  const harbhajanimg = document.getElementById("harbhajan-img");
+  const harbhajan = document.getElementById("Harbhajan Singh");
+  harbhajan.addEventListener("click", function () {
+    harbhajanimg.style.display = "block";
+  });
+
+  const zaheerimg = document.getElementById("zaheer-img");
+  const zaheer = document.getElementById("Zaheer Khan");
+  zaheer.addEventListener("click", function () {
+    zaheerimg.style.display = "block";
+  });
+
+  const munafimg = document.getElementById("munaf-img");
+  const munaf = document.getElementById("Munaf Patel");
+  munaf.addEventListener("click", function () {
+    munafimg.style.display = "block";
+  });
+
+  const sreesanthimg = document.getElementById("sreesanth-img");
+  const sreesanth = document.getElementById("S. Sreesanth");
+  sreesanth.addEventListener("click", function () {
+    sreesanthimg.style.display = "block";
+  });
